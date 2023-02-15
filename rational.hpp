b1abcca5abceb1c43b29c4d46b316e80aa8a91da
Sergey Sargsyan <78570620+Serge-sudo@users.noreply.github.com>
2023-02-15 19:42:11 +0300

    Update rational.hpp

--- rational.hpp
+++ rational.hpp
@@ -1,75 +1,68 @@
+#include<string>
+#include<iostream>
+
 class Rational {
-private:
-    int32_t numerator_;
-    int32_t denominator_;
+public:
+    int32_t numerator_{1};
+    int32_t denominator_{1};
 
     void reduce();
 
-public:
-    class ZeroDivisionException : public std::exception {
-    private:
-        std::string message;
-    public:
-        ZeroDivisionException(const std::string &message);
-        const char *what() const noexcept;
-    };
+    Rational() = default;
 
-    Rational();
+    Rational(const int32_t&);
 
-    Rational(const int32_t &);
+    Rational(const Rational& m) = default;
 
-    Rational(const Rational &m) = default;
-
-    Rational(Rational &&m) = default;
+    Rational(Rational&& m) = default;
 
     ~Rational() = default;
 
-    Rational &operator=(const Rational &) = default;
+    Rational& operator=(const Rational&) = default;
 
-    Rational &operator=(Rational &&) = default;
+    Rational& operator=(Rational&&) = default;
 
-    Rational(const int32_t &,
-             const int32_t &);
-    
-    int32_t getNumerator() const;
+    Rational(const int32_t&,
+             const int32_t&) noexcept(false);
 
-    int32_t getDenominator() const;
+    int32_t numerator() const;
 
-    bool operator==(const Rational &) const;
+    int32_t denominator() const;
 
-    bool operator!=(const Rational &) const;
+    bool operator==(const Rational&) const;
 
-    bool operator>(const Rational &) const;
+    bool operator!=(const Rational&) const;
 
-    bool operator<(const Rational &) const;
+    bool operator>(const Rational&) const;
 
-    bool operator>=(const Rational &) const;
+    bool operator<(const Rational&) const;
 
-    bool operator<=(const Rational &) const;
+    bool operator>=(const Rational&) const;
+
+    bool operator<=(const Rational&) const;
 
     Rational operator-() const;
 
     explicit operator bool() const;
 
-    Rational &operator*=(const Rational &);
+    Rational& operator*=(const Rational&);
 
-    Rational &operator+=(const Rational &);
+    Rational& operator+=(const Rational&);
 
-    Rational &operator-=(const Rational &);
+    Rational& operator-=(const Rational&);
 
-    Rational &operator/=(const Rational &);
-
+    Rational& operator/=(const Rational&) noexcept(false);
 
 };
 
-std::istream &operator>>(std::istream &in, Rational &r);
+std::istream& operator>>(std::istream& in, Rational& r);
 
-std::ostream &operator<<(std::ostream &out, const Rational &r);
+std::ostream& operator<<(std::ostream& out, const Rational& r);
 
-Rational operator/(const Rational &a, const Rational &b);
+Rational operator/(const Rational& a, const Rational& b);
 
-Rational operator+(const Rational &a, const Rational &b);
+Rational operator+(const Rational& a, const Rational& b);
 
-Rational operator*(const Rational &a, const Rational &b);
+Rational operator*(const Rational& a, const Rational& b);
 
-Rational operator-(const Rational &a, const Rational &b);
+Rational operator-(const Rational& a, const Rational& b);
